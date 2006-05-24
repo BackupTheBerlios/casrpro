c669d4f3c007f48b32daba98d40a7fb67f870ace
mandrilock <mandrilock>
2006-05-24 02:33:13 +0000

    Added proposed fields

--- survey/src/java/ar/com/survey/model/PersonAbstract.java
+++ survey/src/java/ar/com/survey/model/PersonAbstract.java
@@ -8,6 +8,21 @@ import java.io.Serializable;
 public abstract class PersonAbstract implements Serializable{
 
 	private static final long serialVersionUID = 1L;
+	private String email;
+	private String emailConfirm;
+	private String firstName;
+	private String lastName;
+	private String state;
+	private String postalCode;
+	private String birthDay, birthMonth, birthYear;
+	private String sex;
+	private String married;
+	private String isEmployed;
+	private String educationLevel;
+	private String ocupation;
+	private String firstAge, secondAge, thirdAge, forthAge, fifthAge;
+	private String firstSex, secondSex, thirdSex, forthSex, fifthSex;
+	private boolean acceptTerms;
 	
 	
 }
