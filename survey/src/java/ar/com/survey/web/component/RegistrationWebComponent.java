e6b78634002116861154ab1621c758b41f79353f
mandrilock <mandrilock>
2006-06-12 17:46:32 +0000

    Updated registration components

---
+++ survey/src/java/ar/com/survey/web/component/RegistrationWebComponent.java
@@ -0,0 +1,68 @@
+package ar.com.survey.web.component;
+
+import java.util.Calendar;
+
+import ar.com.survey.model.Person;
+import ar.com.survey.web.struts.form.RegisterForm;
+
+public class RegistrationWebComponent {
+
+	public RegistrationWebComponent() {
+		
+	}
+	
+	public Person getRegistrationFromForm(RegisterForm rfm){
+		
+		Person person = new Person();
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.DAY_OF_MONTH, Integer.parseInt(rfm.getBirthDay()));
+		calendar.set(Calendar.MONTH, Integer.parseInt(rfm.getBirthMonth()));
+		calendar.set(Calendar.YEAR, Integer.parseInt(rfm.getBirthYear()));
+		person.setBirthDate(calendar);
+		person.setEducationLevel(rfm.getEducationLevel());
+		person.setEmail(rfm.getEmail());
+		//TODO Sebastian: Reemplazar el isEmployed de registracion por un string que sea tipo de employment
+		// registration.setEmployed(rfm.getEmploymentType());
+		
+		// check for the different kids, if they exist persist each
+		
+		// TODO Sebastián: Remover firstName, secondName, thirdName, forthName, FifthName
+		// ya que el form ultimo no los pide
+		
+		if(rfm.getFirstAge()!=null && !rfm.getFirstAge().equals("")){
+			person.setFirstAge(Short.parseShort(rfm.getFirstAge()));
+			person.setFirstSex(rfm.getFirstSex());
+		}
+		
+		if(rfm.getSecondAge()!=null && !rfm.getSecondAge().equals("")){
+			person.setSecondAge(Short.parseShort(rfm.getSecondAge()));
+			person.setSecondSex(rfm.getSecondSex());	
+		}
+		
+		if(rfm.getThirdAge()!=null && !rfm.getThirdAge().equals("")){
+			person.setThirdAge(Short.parseShort(rfm.getThirdAge()));
+			person.setThirdSex(rfm.getThirdSex());	
+		}
+		
+		if(rfm.getForthAge()!=null && !rfm.getForthAge().equals("")){
+			person.setForthAge(Short.parseShort(rfm.getForthAge()));
+			person.setForthSex(rfm.getForthSex());
+		}
+		
+		if(rfm.getFifthAge()!=null && !rfm.getFifthAge().equals("")){
+			person.setFifthAge(Short.parseShort(rfm.getFifthAge()));
+			person.setFifthSex(rfm.getFifthAge());
+		}
+		
+		person.setLastName(rfm.getLastName());
+		person.setMaritalStatus(rfm.getMaritalStatus());
+		person.setOcupation(rfm.getOcupation());
+		person.setPostalCode(rfm.getPostalCode());
+		
+		person.setSex(rfm.getSex());
+		person.setState(rfm.getState());
+		
+		return person;
+	}
+
+}
--- survey/src/java/ar/com/survey/web/struts/action/RegisterAction.java
+++ survey/src/java/ar/com/survey/web/struts/action/RegisterAction.java
@@ -12,53 +12,59 @@ import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.actions.DispatchAction;
 
-import ar.com.survey.model.Registration;
+import ar.com.survey.model.Person;
 import ar.com.survey.registration.IRegistrationComponent;
 import ar.com.survey.registration.PersonExistsException;
 import ar.com.survey.registration.RegistrationExistsException;
-import ar.com.survey.web.component.RegistrationComponent;
+import ar.com.survey.web.component.RegistrationWebComponent;
 import ar.com.survey.web.struts.form.RegisterForm;
 
 public class RegisterAction extends DispatchAction {
 
-	private static Logger logger = Logger.getLogger("RegisterAction.class");
+	private static Logger logger = Logger.getLogger(RegisterAction.class);
 
 	// TODO Clauio: creo que voy entendiendo cómo Spring la inyetca los services
 	// a cada action
 	// pero... esta variable es de clase y struts reutiliza los actions, los
 	// cuales deben ser thread-safe ...
 	// entonces, este service también deberia ser thread safe????
-	// si pasa eso con todos, puede llegar a ser una cagad... se puede hacer distinto?
+	// si pasa eso con todos, puede llegar a ser una cagad... se puede hacer
+	// distinto?
 
 	private IRegistrationComponent registerFacade;
-
-	public ActionForward execute(ActionMapping mapping, ActionForm form,
-			HttpServletRequest request, HttpServletResponse response)
-			throws Exception {
-		return mapping.findForward("registerForm");
-	}
 
 	public ActionForward register(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response)
 			throws IOException, ServletException {
 
 		RegisterForm rfm = (RegisterForm) form;
-		Registration registration = new RegistrationComponent().getRegistrationFromForm(rfm);
-		
+		Person person = new RegistrationWebComponent()
+				.getRegistrationFromForm(rfm);
+		ActionForward forward = null;
 		try {
-			registerFacade.register(registration);
+			registerFacade.register(person);
+			forward = mapping.findForward("success");
 		} catch (RegistrationExistsException e) {
-			// TODO Claudio forward al error con el msg correspondiente, le
-			// damos la opcion de hacer un resend de la notificación?..
-			// si si, entonces:
-			// implementare un metodo mas en este componente
-			// (resendNotificacion(email)
-			// y reseteamos la fecha de regsitracion para que dure N dias más
+			forward = mapping.findForward("error");
+			// TODO Sebastián aca habría que reenviarle el mail y como decías
+			// resetearle la fecha
+			// Por otro lado sabes como manejar el tema de las fechas, osea como
+			// hacer a partir
+			// de una fecha dada como saber si ya pasaron n días o no... te
+			// pregunto porque no
+			// conozco una forma de hacerlo sin hacer el calculo a mano...
 		} catch (PersonExistsException e) {
-			// TODO Claudio forward al error con el msg correspondiente
+			forward = mapping.findForward("exists");
 		}
+		return forward;
+	}
 
-		return mapping.findForward("success");
+	protected ActionForward unspecified(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse reponse)
+			throws Exception {
+
+		logger.info("Show register form");
+		return mapping.findForward("registerForm");
 	}
 
 	public void setRegisterFacade(IRegistrationComponent registerFacade) {
