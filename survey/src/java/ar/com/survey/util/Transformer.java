db811ecc7f2c691a8750674556b62c896258c887
mandrilock <mandrilock>
2006-07-20 20:18:46 +0000

    Transformation Utility class, empleada para transformar tipos de objetos

---
+++ survey/src/java/ar/com/survey/util/Transformer.java
@@ -0,0 +1,13 @@
+package ar.com.survey.util;
+
+import java.util.Calendar;
+
+public abstract class Transformer {
+
+	public static Calendar getCalendarFromString(String date){
+		// TODO Claudio: implementar el codigo real del metodo
+		Calendar calendar = Calendar.getInstance();
+		return calendar;
+	}
+	
+}
