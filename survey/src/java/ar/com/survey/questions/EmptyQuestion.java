16aeb31c93964f8d194845c4ad79cd76abe1eead
sesponda <sesponda>
2006-08-16 18:16:20 +0000

    *** empty log message ***

---
+++ survey/src/java/ar/com/survey/questions/EmptyQuestion.java
@@ -0,0 +1,17 @@
+package ar.com.survey.questions;
+
+import ar.com.survey.model.Question;
+
+
+
+public class EmptyQuestion extends Question {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public EmptyQuestion() {
+		super();
+	}
+}
