874c7fd17c8d92d5f0ebbf02ef653c2765b0731c
mandrilock <mandrilock>
2006-08-25 21:09:24 +0000

    web survey management component

--- survey/src/java/ar/com/survey/web/component/SurveyWebComponent.java
+++ survey/src/java/ar/com/survey/web/component/SurveyWebComponent.java
@@ -1,5 +1,6 @@
 package ar.com.survey.web.component;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Iterator;
 import java.util.List;
@@ -8,12 +9,18 @@ import java.util.Set;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import ar.com.survey.DummySurveyComponent;
-import ar.com.survey.ISurveyComponent;
+import ar.com.survey.admin.ISurveyComponent;
+import ar.com.survey.admin.SurveyComponent;
 import ar.com.survey.model.Question;
 import ar.com.survey.model.Quota;
 import ar.com.survey.model.Section;
 import ar.com.survey.model.Survey;
+import ar.com.survey.model.enums.SurveyState;
+import ar.com.survey.questions.EmptyQuestion;
+import ar.com.survey.questions.list.CheckBoxListQuestion;
+import ar.com.survey.questions.list.NumberListQuestion;
+import ar.com.survey.questions.list.StringListQuestion;
+import ar.com.survey.questions.matrix.RadioMatrixQuestion;
 import ar.com.survey.questions.single.StringQuestion;
 import ar.com.survey.questions.single.TextAreaQuestion;
 import ar.com.survey.util.Transformer;
@@ -24,8 +31,7 @@ public class SurveyWebComponent {
 	private ISurveyComponent surveyComponent;
 
 	public SurveyWebComponent(){
-		// TODO Sebastián: Replace the dummy for the real object value
-		surveyComponent = new DummySurveyComponent();
+		surveyComponent = new SurveyComponent();
 	}
 	
 	/**
@@ -37,7 +43,21 @@ public class SurveyWebComponent {
 	public void createSessionSurvey(HttpServletRequest request){
 		HttpSession session = request.getSession(true);
 		Survey survey = new Survey();
+		survey.setCreationDate(Calendar.getInstance());
+		survey.setStatus(SurveyState.OPEN.getCode());
 		session.setAttribute("currentSurvey", survey);
+	}
+	
+	/**
+	 * 
+	 * This method removes a persisted survey object 
+	 * 
+	 * @param form used to remove the persisted survey
+	 */
+	public void removePersistedSurvey(SurveyForm form){
+		Survey survey = new Survey();
+		survey.setName(form.getName()); 
+		surveyComponent.deleteSurvey(survey);
 	}
 	
 	/**
@@ -153,13 +173,13 @@ public class SurveyWebComponent {
 		session.setAttribute("currentSurvey", survey);
 	}
 	
+	/* Question management methods */
+	
 	public void addOpenQuestionToSection(HttpServletRequest request){
 		HttpSession session = request.getSession();
 		Section section = (Section) session.getAttribute("currentSection");
 		String name = request.getParameter("name");
 		String image = request.getParameter("image");
-		
-		// TODO Claudio: verificar que pasa con la imagen, donde se persiste...
 		
 		String questionText = request.getParameter("quesTxt");
 		String answerType = request.getParameter("txtType");
@@ -175,12 +195,368 @@ public class SurveyWebComponent {
 		question.setDescription(questionText);
 		question.setSection(section);
 		question.setTitle(name);
+		question.setImage(image);
 
 		List<Question> quests = section.getQuestions();
 		quests.add(question);
 		
 		section.setQuestions(quests);
 		
+		session.setAttribute("currentSection", section);
+	}
+	
+	public void updateOpenQuestionInSection(HttpServletRequest request){
+		HttpSession session = request.getSession();
+		Section section = (Section) session.getAttribute("currentSection");
+		String name = request.getParameter("name");
+		String image = request.getParameter("image");
+		
+		String questionText = request.getParameter("quesTxt");
+		String answerType = request.getParameter("txtType");
+		int rowId = Integer.parseInt(request.getParameter("row"));
+		rowId--;
+		
+		// now parse different params depending on the type
+		Question question = null;
+		
+		if(answerType.equals("textArea"))
+			question = new TextAreaQuestion();
+		else
+			question = new StringQuestion();
+		
+		question.setDescription(questionText);
+		question.setSection(section);
+		question.setTitle(name);
+		question.setImage(image);
+
+		List<Question> quests = section.getQuestions();
+		quests.set(rowId, question);
+		
+		section.setQuestions(quests);
+		
+		session.setAttribute("currentSection", section);
+	}
+	
+	public void addEmptyQuestionToSection(HttpServletRequest request){
+		HttpSession session = request.getSession();
+		Section section = (Section) session.getAttribute("currentSection");
+		String name = request.getParameter("name");
+		String image = request.getParameter("image");
+		
+		String questionText = request.getParameter("quesTxt");
+		
+		// now parse different params depending on the type
+		Question question = new EmptyQuestion();
+				
+		question.setDescription(questionText);
+		question.setSection(section);
+		question.setTitle(name);
+		question.setImage(image);
+
+		List<Question> quests = section.getQuestions();
+		quests.add(question);
+		
+		section.setQuestions(quests);
+		
+		session.setAttribute("currentSection", section);
+	}
+	
+	public void updateEmptyQuestionInSection(HttpServletRequest request){
+		HttpSession session = request.getSession();
+		Section section = (Section) session.getAttribute("currentSection");
+		String name = request.getParameter("name");
+		String image = request.getParameter("image");
+		
+		String questionText = request.getParameter("quesTxt");
+		int rowId = Integer.parseInt(request.getParameter("row"));
+		rowId--;
+		
+		// now parse different params depending on the type
+		Question question = new EmptyQuestion();
+		
+		question.setDescription(questionText);
+		question.setSection(section);
+		question.setTitle(name);
+		question.setImage(image);
+
+		List<Question> quests = section.getQuestions();
+		quests.set(rowId, question);
+		
+		section.setQuestions(quests);
+		
+		session.setAttribute("currentSection", section);
+	}
+	
+	public void addNumericQuestionToSection(HttpServletRequest request){
+		HttpSession session = request.getSession();
+		Section section = (Section) session.getAttribute("currentSection");
+		String name = request.getParameter("name");
+		String image = request.getParameter("image");
+		String validationType = request.getParameter("validationType");
+		String min = request.getParameter("min");
+		String max = request.getParameter("max");
+		String total = request.getParameter("total");
+		
+		String questionText = request.getParameter("questionTxt");
+		
+		ArrayList<String> answers = (ArrayList<String>) session.getAttribute("answers");
+		
+		// now parse different params depending on the type
+		NumberListQuestion question = new NumberListQuestion(answers.size());
+		
+		question.setDescription(questionText);
+		question.setSection(section);
+		question.setTitle(name);
+		question.setItems(answers);
+		
+		if(validationType.equals("individual")){
+			question.setMin(Integer.parseInt(min));
+			question.setMax(Integer.parseInt(max));
+		} else if (validationType.equals("total")){
+			question.setTotal(Integer.parseInt(total));
+		}
+		question.setImage(image);
+
+
+		List<Question> quests = section.getQuestions();
+		quests.add(question);
+		
+		section.setQuestions(quests);
+		
+		session.setAttribute("currentSection", section);
+		session.removeAttribute("answers");
+	}
+	
+	public void updateNumericQuestionInSection(HttpServletRequest request){
+		HttpSession session = request.getSession();
+		Section section = (Section) session.getAttribute("currentSection");
+		String name = request.getParameter("name");
+		String image = request.getParameter("image");
+		String validationType = request.getParameter("validationType");
+		String min = request.getParameter("min");
+		String max = request.getParameter("max");
+		String total = request.getParameter("total");
+		
+		String questionText = request.getParameter("questionTxt");
+		int rowId = Integer.parseInt(request.getParameter("row"));
+		rowId--;
+		
+		ArrayList<String> answers = (ArrayList<String>) session.getAttribute("answers");
+		
+		// now parse different params depending on the type
+		NumberListQuestion question = new NumberListQuestion(answers.size());
+		
+		question.setDescription(questionText);
+		question.setSection(section);
+		question.setTitle(name);
+		question.setItems(answers);
+		if(validationType.equals("individual")){
+			question.setMin(Integer.parseInt(min));
+			question.setMax(Integer.parseInt(max));
+		} else if (validationType.equals("total")){
+			question.setTotal(Integer.parseInt(total));
+		}
+		question.setImage(image);
+
+
+		List<Question> quests = section.getQuestions();
+		quests.set(rowId, question);
+		
+		section.setQuestions(quests);
+		
+		session.setAttribute("currentSection", section);
+		session.removeAttribute("answers");
+	}
+	
+	public void addStringListQuestionToSection(HttpServletRequest request){
+		HttpSession session = request.getSession();
+		Section section = (Section) session.getAttribute("currentSection");
+		String name = request.getParameter("name");
+		String image = request.getParameter("image");
+		
+		String questionText = request.getParameter("questionTxt");
+		ArrayList<String> answers = (ArrayList<String>) session.getAttribute("answers");
+		
+		// now parse different params depending on the type
+		StringListQuestion question = new StringListQuestion();
+		
+		question.setDescription(questionText);
+		question.setSection(section);
+		question.setTitle(name);
+		question.setItems(answers);
+		question.setImage(image);
+
+		List<Question> quests = section.getQuestions();
+		quests.add(question);
+		
+		section.setQuestions(quests);
+		
+		session.setAttribute("currentSection", section);
+		session.removeAttribute("answers");
+	}
+	
+	public void updateStringListQuestionInSection(HttpServletRequest request){
+		HttpSession session = request.getSession();
+		Section section = (Section) session.getAttribute("currentSection");
+		String name = request.getParameter("name");
+		String image = request.getParameter("image");
+		
+		String questionText = request.getParameter("questionTxt");
+		int rowId = Integer.parseInt(request.getParameter("row"));
+		rowId--;
+		
+		ArrayList<String> answers = (ArrayList<String>) session.getAttribute("answers");
+		
+		// now parse different params depending on the type
+		StringListQuestion question = new StringListQuestion();
+		
+		question.setDescription(questionText);
+		question.setSection(section);
+		question.setTitle(name);
+		question.setItems(answers);
+		question.setImage(image);
+
+		List<Question> quests = section.getQuestions();
+		quests.set(rowId, question);
+		
+		section.setQuestions(quests);
+		session.setAttribute("currentSection", section);
+		session.removeAttribute("answers");
+	}
+	
+	public void addCheckBoxQuestionToSection(HttpServletRequest request){
+		HttpSession session = request.getSession();
+		Section section = (Section) session.getAttribute("currentSection");
+		String name = request.getParameter("name");
+		String image = request.getParameter("image");
+		
+		String questionText = request.getParameter("questionTxt");
+		
+		ArrayList<String> answers = (ArrayList<String>) session.getAttribute("answers");
+		
+		// now parse different params depending on the type
+		CheckBoxListQuestion question = new CheckBoxListQuestion();
+		
+		question.setDescription(questionText);
+		question.setSection(section);
+		question.setTitle(name);
+		question.setItems(answers);
+		question.setImage(image);
+
+		List<Question> quests = section.getQuestions();
+		quests.add(question);
+		
+		section.setQuestions(quests);
+		session.setAttribute("currentSection", section);
+		session.removeAttribute("answers");
+	}
+	
+	public void updateCheckBoxQuestionInSection(HttpServletRequest request){
+		HttpSession session = request.getSession();
+		Section section = (Section) session.getAttribute("currentSection");
+		String name = request.getParameter("name");
+		String image = request.getParameter("image");
+		
+		String questionText = request.getParameter("questionTxt");
+		int rowId = Integer.parseInt(request.getParameter("row"));
+		rowId--;
+		
+		ArrayList<String> answers = (ArrayList<String>) session.getAttribute("answers");
+		
+		// now parse different params depending on the type
+		CheckBoxListQuestion question = new CheckBoxListQuestion();
+		
+		question.setDescription(questionText);
+		question.setSection(section);
+		question.setTitle(name);
+		question.setItems(answers);
+		question.setImage(image);
+
+		List<Question> quests = section.getQuestions();
+		quests.set(rowId, question);
+		
+		section.setQuestions(quests);
+		session.setAttribute("currentSection", section);
+		session.removeAttribute("answers");
+	}
+	
+	
+	public void addMatrixQuestionToSection(HttpServletRequest request){
+		HttpSession session = request.getSession();
+		Section section = (Section) session.getAttribute("currentSection");
+		String name = request.getParameter("name");
+		String image = request.getParameter("image");
+		
+		String questionText = request.getParameter("questionTxt");
+		
+		ArrayList<String> answers = (ArrayList<String>) session.getAttribute("answers");
+		ArrayList<String> columns = (ArrayList<String>) session.getAttribute("columns");
+		
+		// now parse different params depending on the type
+		RadioMatrixQuestion question = new RadioMatrixQuestion(columns.size(), answers.size());
+		
+		question.setDescription(questionText);
+		question.setSection(section);
+		question.setTitle(name);
+		question.setItems(answers);
+		question.setColumnsTitles(columns);
+		question.setImage(image);
+
+		List<Question> quests = section.getQuestions();
+		quests.add(question);
+		
+		section.setQuestions(quests);
+		
+		session.setAttribute("currentSection", section);
+		session.removeAttribute("answers");
+		session.removeAttribute("columns");
+	}
+	
+	public void updateMatrixQuestionInSection(HttpServletRequest request){
+		HttpSession session = request.getSession();
+		Section section = (Section) session.getAttribute("currentSection");
+		String name = request.getParameter("name");
+		String image = request.getParameter("image");
+		
+		String questionText = request.getParameter("questionTxt");
+		int rowId = Integer.parseInt(request.getParameter("row"));
+		rowId--;
+		
+		ArrayList<String> answers = (ArrayList<String>) session.getAttribute("answers");
+		ArrayList<String> columns = (ArrayList<String>) session.getAttribute("columns");
+		
+		// now parse different params depending on the type
+		RadioMatrixQuestion question = new RadioMatrixQuestion(columns.size(), answers.size());
+		
+		question.setDescription(questionText);
+		question.setSection(section);
+		question.setTitle(name);
+		question.setItems(answers);
+		question.setColumnsTitles(columns);
+		question.setImage(image);
+
+		List<Question> quests = section.getQuestions();
+		quests.set(rowId, question);
+		
+		section.setQuestions(quests);
+		
+		session.setAttribute("currentSection", section);
+		session.removeAttribute("answers");
+		session.removeAttribute("columns");
+	}
+	
+	
+	
+	public void removeQuestionInSection(HttpServletRequest request){
+		HttpSession session = request.getSession();
+		Section section = (Section) session.getAttribute("currentSection");
+		int rowId = Integer.parseInt(request.getParameter("row"));
+		rowId--;
+		
+		List<Question> quests = section.getQuestions();
+		quests.remove(rowId);
+		
+		section.setQuestions(quests);
 		session.setAttribute("currentSection", section);
 	}
 	
@@ -197,54 +573,67 @@ public class SurveyWebComponent {
 		return question;
 	}
 	
-	public void updateOpenQuestionInSection(HttpServletRequest request){
+	/* answer and columns management methods */
+	
+	public void addAnswerToSession(HttpServletRequest request){
 		HttpSession session = request.getSession();
-		Section section = (Section) session.getAttribute("currentSection");
-		String name = request.getParameter("name");
-		String image = request.getParameter("image");
-		
-		
-		// TODO Claudio: verificar que pasa con la imagen, donde se persiste...
-		
-		String questionText = request.getParameter("quesTxt");
-		String answerType = request.getParameter("txtType");
-		int rowId = Integer.parseInt(request.getParameter("row"));
-		rowId--;
-		
-		// now parse different params depending on the type
-		Question question = null;
-		
-		if(answerType.equals("textArea"))
-			question = new TextAreaQuestion();
+		String answer = request.getParameter("answer");
+		ArrayList<String> answers;
+		if(session.getAttribute("answers")==null)
+			 answers = new ArrayList<String>();
 		else
-			question = new StringQuestion();
-		
-		question.setDescription(questionText);
-		question.setSection(section);
-		question.setTitle(name);
-
-		List<Question> quests = section.getQuestions();
-		quests.set(rowId, question);
-		
-		section.setQuestions(quests);
-		
-		session.setAttribute("currentSection", section);
+			 answers = (ArrayList<String>) session.getAttribute("answers");
+		answers.add(answer);
+		session.setAttribute("answers", answers);
 	}
 	
-	/*
-	public void addQuestionToSessionSection(HttpServletRequest request){
+	public void removeAnswerFromSession(HttpServletRequest request){
 		HttpSession session = request.getSession();
-		Section section = (Section) session.getAttribute("currentSection");
-		String name = request.getParameter("name");
-		String image = request.getParameter("image");
-		String questionText = request.getParameter("question");
-		String questionType = request.getParameter("type");
-		
-		// now parse different params depending on the type
-		
-		// Question question = new Question(section);
-		
-		session.setAttribute("currentSection", section);
-	} */
+		int rowId = Integer.parseInt(request.getParameter("row"));
+		rowId--;
+		ArrayList<String> answers = (ArrayList<String>) session.getAttribute("answers");
+		answers.remove(rowId);
+		session.setAttribute("answers", answers);
+	}
 	
+	public void updateAnswerInSession(HttpServletRequest request){
+		HttpSession session = request.getSession();
+		int rowId = Integer.parseInt(request.getParameter("row"));
+		String answer = request.getParameter("answer");
+		rowId--;
+		ArrayList<String> answers = (ArrayList<String>) session.getAttribute("answers");
+		answers.set(rowId, answer);
+		session.setAttribute("answers", answers);
+	}
+	
+	public void addColumnToSession(HttpServletRequest request){
+		HttpSession session = request.getSession();
+		String column = request.getParameter("column");
+		ArrayList<String> columns;
+		if(session.getAttribute("columns")==null)
+			columns = new ArrayList<String>();
+		else
+			columns = (ArrayList<String>) session.getAttribute("columns");
+		columns.add(column);
+		session.setAttribute("columns", columns);
+	}
+	
+	public void removeColumnFromSession(HttpServletRequest request){
+		HttpSession session = request.getSession();
+		int rowId = Integer.parseInt(request.getParameter("row"));
+		rowId--;
+		ArrayList<String> columns = (ArrayList<String>) session.getAttribute("columns");
+		columns.remove(rowId);
+		session.setAttribute("columns", columns);
+	}
+	
+	public void updateColumnInSession(HttpServletRequest request){
+		HttpSession session = request.getSession();
+		int rowId = Integer.parseInt(request.getParameter("row"));
+		String column = request.getParameter("column");
+		rowId--;
+		ArrayList<String> columns = (ArrayList<String>) session.getAttribute("columns");
+		columns.set(rowId, column);
+		session.setAttribute("columns", columns);
+	}
 }
