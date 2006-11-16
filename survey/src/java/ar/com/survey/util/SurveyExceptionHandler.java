8abe351a6db56e4a5368e6f3efa689dcc13e777e
mandrilock <mandrilock>
2006-11-16 15:59:23 +0000

    Custom Error Handler for Struts

---
+++ survey/src/java/ar/com/survey/util/SurveyExceptionHandler.java
@@ -0,0 +1,28 @@
+package ar.com.survey.util;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ExceptionHandler;
+import org.apache.struts.config.ExceptionConfig;
+
+public class SurveyExceptionHandler extends ExceptionHandler {
+
+	private static final Logger log = Logger.getLogger(ExceptionHandler.class);
+
+	@Override
+	public ActionForward execute(Exception exception,
+			ExceptionConfig exceptionConfig, ActionMapping mapping,
+			ActionForm form, HttpServletRequest request,
+			HttpServletResponse response) throws ServletException {
+
+		log.error(exception);
+		return super.execute(exception, exceptionConfig, mapping, form, request, response);
+	}
+
+}
