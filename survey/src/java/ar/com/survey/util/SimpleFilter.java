370f71080c8bb0a1b01d85401e15f23bb8737720
mandrilock <mandrilock>
2006-12-06 18:51:50 +0000

    Simple filter object used to create reports filtered

---
+++ survey/src/java/ar/com/survey/util/SimpleFilter.java
@@ -0,0 +1,29 @@
+package ar.com.survey.util;
+
+public class SimpleFilter {
+
+	private String filterName;
+	private String filterValue;
+	
+	public SimpleFilter() {
+	}
+
+	public String getFilterName() {
+		return filterName;
+	}
+
+	public void setFilterName(String filterName) {
+		this.filterName = filterName;
+	}
+
+	public String getFilterValue() {
+		return filterValue;
+	}
+
+	public void setFilterValue(String filterValue) {
+		this.filterValue = filterValue;
+	}
+	
+	
+	
+}
